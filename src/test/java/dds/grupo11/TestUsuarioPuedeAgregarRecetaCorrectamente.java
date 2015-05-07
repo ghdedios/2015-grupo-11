d754a1739bdf7e8dfde566184b97c6030ab3924a
Gonzalo de Dios <ghdedios@hotmail.com>
2015-05-07 00:38:23 -0300

    Test punto 3 de la entrega 1

---
+++ src/test/java/dds/grupo11/TestUsuarioPuedeAgregarRecetaCorrectamente.java
@@ -0,0 +1,27 @@
+package dds.grupo11;
+
+import java.time.LocalDate;
+import static org.junit.Assert.*; 
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUsuarioPuedeAgregarRecetaCorrectamente {
+	
+	private Usuario usuario;
+	private Receta receta;
+	
+	@Before
+	public void setUp(){
+		usuario = new Usuario("Usuario de prueba",1.80,75,LocalDate.of(2000,1,1),"Hombre");
+		receta = new Receta(false,"Receta de prueba","Dificil","Explicacion de prueba","Otoño");
+		usuario.agregarReceta(receta);
+
+	}
+	
+	@Test
+	public void usuarioPuedeAgregarReceta() throws Exception{
+		assertSame(receta,usuario.getRecetas().stream().findFirst().get());
+	}
+
+}
